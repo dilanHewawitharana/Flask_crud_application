5bea9c7ac0e5be6a710370d6e839729bd10b2f7a
Dilan Sarith Hewawitharana <dshewawitharana@gmail.com>
2024-07-23 22:17:19 +0900

    data base created

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    making_time = db.Column(db.DateTime, default=datetime.utcnow)
+    serves = db.Column(db.String(200), nullable=False)
+    ingredients = db.Column(db.String(200), nullable=False)
+    cost = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+ 
+    def __repr__(self):
+        return '<Recipe %r' % self.id
+
 
 @app.route('/')
 def index():
